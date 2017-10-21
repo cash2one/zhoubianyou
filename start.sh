4be3db03d1f55879254ccbfd00194117a1fd13ee
root <root@haomantech.(none)>
2017-10-21 16:41:50 +0800

    FIX: start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 BASE_DIR=`pwd`
 
-docker run -d -v ${BASE_DIR}/data:/code/data -p "8000:8000" zuobianyou/collector:latest
+docker run -d --user $(id -u) -v ${BASE_DIR}/data:/code/data -p "8000:8000" zuobianyou/collector:latest
