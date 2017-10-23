242f9dea763816085bb1e07eb7c12f29e3f3c678
lbjworld <lbj.world@gmail.com>
2017-10-23 17:31:16 +0800

    FIX: update proxy

--- collector/update_proxy.py
+++ collector/update_proxy.py
@@ -5,7 +5,6 @@
 
 from pyspider.libs.base_handler import *
 import logging
-import functools
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +18,6 @@ class Handler(BaseHandler):
 
     PROXY_USERS = ['nf_zhoubianyou', 'gat_zhoubianyou', 'comment_fetcher']
     PAGES = 5  # 只抓取5页匿名代理
-    PROXY_POOL = []
     TEST_URL = 'http://www.baidu.com'
 
     @every(minutes=60)
@@ -41,18 +39,25 @@ class Handler(BaseHandler):
             ip = proxy_item(ip_col_selector).text()
             port = proxy_item(port_col_selector).text()
             http_type = proxy_item(type_col_selector).text()
-            if http_type and http_type.low() == 'http':
+            if http_type and http_type.lower() == 'http':
                 proxy_host = '{ip}:{port}'.format(ip=ip, port=port)
                 # test proxy
-                bind_func = functools.partial(self.test_proxy_result, proxy_host=proxy_host)
-                self.crawl(self.TEST_URL, proxy=proxy_host, callback=bind_func)
+                self.crawl(
+                    self.TEST_URL,
+                    proxy=proxy_host,
+                    callback=self.test_proxy_result,
+                    save={
+                        'proxy_host': proxy_host
+                    }
+                )
 
     @catch_status_code_error
-    def test_proxy_result(self, proxy_host, response):
-        if response.status_code == 200:
+    def test_proxy_result(self, response):
+        proxy_host = response.save.get('proxy_host')
+        if response.ok:
             # test success
-            PROXY_POOL.append(proxy_host)
             for project in self.PROXY_USERS:
                 self.send_message(project, proxy_host, url=proxy_host)
+            return {'proxy': proxy_host}
         # test fail, ignore proxy_host
         logger.warn('test proxy fail, ignore: {host}'.format(host=proxy_host))
